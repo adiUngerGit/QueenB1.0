a7b5230efb25287d01f56915c7e958bca90d71dd
rutipop <ruti.popilov@gmail.com>
2020-09-08 20:08:07 +0300

    adding icon and the contact us activity

---
+++ app/src/main/java/com/example/queenb/contact_us.kt
@@ -0,0 +1,12 @@
+package com.example.queenb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contact_us : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+    }
+}
